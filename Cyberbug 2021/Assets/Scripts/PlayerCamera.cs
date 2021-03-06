af2e3b33951f6b66ecb1a104d3f933cda2d1e53c
Erdős Tamás <AG78KX@inf.elte.hu>
2021-03-06 18:55:09 +0100

    Kamera mozgatás implementálva

---
+++ Cyberbug 2021/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour{
+    [Header("Setup")] [SerializeField] GameObject playerCamera;
+
+    [Header("Settings")] [SerializeField] float cameraSensitivity = 5f;
+
+    Vector2 mouseDelta = Vector2.zero;
+    float targetVerticalRotation = 0.0f;
+    
+    void Awake(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate(){
+        Vector3 currentRotation = playerCamera.transform.localRotation.eulerAngles;
+
+        float newHorizontal = currentRotation.y + (mouseDelta.x * cameraSensitivity * Time.deltaTime);
+
+        targetVerticalRotation -= ((mouseDelta.y * cameraSensitivity) * Time.deltaTime);
+        targetVerticalRotation = Mathf.Clamp(targetVerticalRotation, -90f, 90f);
+        
+        
+        Vector3 newRotation = new Vector3(targetVerticalRotation, newHorizontal, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(newRotation);
+    }
+
+    // Read mouse movement
+    public void ReceiveMouseDelta(InputAction.CallbackContext context){
+        mouseDelta = context.action.ReadValue<Vector2>();
+    }
+}
