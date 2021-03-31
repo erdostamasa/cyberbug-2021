fcb5cc548f4e15c1280c8779bdd002fe7b3aa2d2
Erdős Tamás <AG78KX@inf.elte.hu>
2021-03-31 15:35:01 +0200

    Unit teszt sebzésre

---
+++ Cyberbug-2021/Assets/Tests/TestSuite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite{
+
+        [UnityTest]
+        public IEnumerator EnemyTakesDamage(){
+            GameObject enemy = new GameObject();
+            EnemyHealth health = enemy.AddComponent<EnemyHealth>();
+            health.MaxHealth = 10;
+            yield return new WaitForEndOfFrame();
+            health.ReceiveProjectile();
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual(9, health.Health);
+            
+            Object.Destroy(enemy);
+        }
+    }
+}
